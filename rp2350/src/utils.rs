c7860c9b6cb80723bad282e714bd4e23f7522f3b
Le Duy Nguyen <xnguye27@stud.fit.vut.cz>
2025-04-27 02:41:59 +0200

    feat: add more utility functions to manipulate bits

--- rp2350/src/utils.rs
+++ rp2350/src/utils.rs
@@ -44,10 +44,90 @@ pub fn set_bit_state(bits: &mut u32, bit: u32, state: bool) {
     }
 }
 
-pub fn set_bit(bits: &mut u32, bit: u32) {
-    *bits |= 1 << bit;
+// write 1 to clear
+pub fn w1c(dst: &mut u32, bits: u32, mask: u32) {
+    let to_clear = bits & mask;
+    *dst &= !to_clear;
 }
 
-pub fn clear_bit(bits: &mut u32, bit: u32) {
-    *bits &= !(1 << bit);
+pub fn clear_bits(bits: &mut u32, mask: u32) {
+    *bits &= !mask;
+}
+
+pub fn set_bit(bits: &mut u32, position: u32) {
+    *bits |= 1 << position;
+}
+
+pub fn clear_bit(bits: &mut u32, position: u32) {
+    *bits &= !(1 << position);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_bit() {
+        let bits: u32 = 0b10101010;
+        assert_eq!(extract_bit(bits, 2), 0);
+        assert_eq!(extract_bit(bits, 3), 1);
+    }
+
+    #[test]
+    fn test_extract_bits() {
+        let bits: u32 = 0b10101010;
+        assert_eq!(extract_bits(bits, 2..=5), 0b1010);
+        assert_eq!(extract_bits(bits, 4..=6), 0b010);
+    }
+
+    #[test]
+    fn test_sign_extend() {
+        assert_eq!(sign_extend(0b11111111, 7), 0xFFFFFFFF);
+        assert_eq!(sign_extend(0b01111111, 7), 0x0000007F);
+    }
+
+    #[test]
+    fn test_set_bit_state() {
+        let mut bits: u32 = 0b00000000;
+        set_bit_state(&mut bits, 2, true);
+        assert_eq!(bits, 0b00000100);
+        set_bit_state(&mut bits, 2, false);
+        assert_eq!(bits, 0b00000000);
+    }
+
+    #[test]
+    fn test_set_bit() {
+        let mut bits: u32 = 0b00000000;
+        set_bit(&mut bits, 2);
+        assert_eq!(bits, 0b00000100);
+        set_bit(&mut bits, 2);
+        assert_eq!(bits, 0b00000100);
+    }
+
+    #[test]
+    fn test_clear_bit() {
+        let mut bits: u32 = 0b00000100;
+        clear_bit(&mut bits, 2);
+        assert_eq!(bits, 0b00000000);
+        clear_bit(&mut bits, 2);
+        assert_eq!(bits, 0b00000000);
+    }
+
+    #[test]
+    fn test_clear_bits() {
+        let mut bits: u32 = 0b11111111;
+        clear_bits(&mut bits, 0b00001111);
+        assert_eq!(bits, 0b11110000);
+        clear_bits(&mut bits, 0b11110000);
+        assert_eq!(bits, 0b00000000);
+    }
+
+    #[test]
+    fn test_w1c() {
+        let mut bits: u32 = 0b11111111;
+        w1c(&mut bits, 0b01001110, 0b10111111);
+        assert_eq!(bits, 0b11110001);
+        w1c(&mut bits, 0b11110000, 0b11111111);
+        assert_eq!(bits, 0b00000001);
+    }
 }
